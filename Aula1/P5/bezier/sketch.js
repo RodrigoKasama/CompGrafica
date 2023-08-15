7153c1e61a9652844d0df0fbe8cdad5142c866bc
RodKasama <rodrigo.parracho@gmail.com>
2023-08-15 20:27:17 -0300

    Aula 2 - Trabalhando com Bezier

--- Aula1/P5/bezier/sketch.js
+++ Aula1/P5/bezier/sketch.js
@@ -2,43 +2,60 @@ let pontos = [];
 let selecionado = null;
 
 function setup() {
-    createCanvas(600,600);
+    createCanvas(800, 600);
     pontos = [
-        createVector(10,height/2), 
-        createVector(width-10,height/2), 
-        createVector(width/2,height-10)
+        createVector(10, height / 2),
+        createVector(width - 10, height / 2),
+        createVector(width / 2, 10)
     ];
-}  
-
-function ponto(A) {
-    circle(A.x,A.y,10);
 }
 
-function combina(A,B,t) {
-    return {x:(1-t)*A.x+t*B.x,y:(1-t)*A.y+t*B.y};
+function ponto(A) {
+    circle(A.x, A.y, 10);
+}
+
+function combina(A, B, t) {
+    return {
+        x: (1 - t) * A.x + t * B.x,
+        y: (1 - t) * A.y + t * B.y
+    };
 }
 
 function draw() {
-    let [p1, p2, p3] = pontos;
+
+    let [A, B, C] = pontos;
+    let D, E, F;
+
     background(200);
     noFill();
+
+    line(A.x, A.y, C.x, C.y)
+    line(C.x, C.y, B.x, B.y)
+
     beginShape();
-    for(let t=0; t<=1.0; t+=0.05) {
-        A = combina(p1,p3,t);
-        B = combina(p3,p2,t);
-        C = combina(A,B,t);
-        vertex(C.x,C.y);
+    for (let t = 0; t <= 1.0; t += 0.05) {
+        D = combina(A, C, t);
+        E = combina(C, B, t);
+        F = combina(D, E, t);
+        vertex(F.x, F.y);
     }
-    vertex(p2.x,p2.y);
+    vertex(B.x, B.y);
     endShape();
+
     desenhaPontos();
 }
 
 function desenhaPontos() {
-    let vmouse = createVector(mouseX,mouseY);
+
+    // Apesar de ser um ponto conceitualmente, mouse é um vetor
+    let vmouse = createVector(mouseX, mouseY);
     selecionado = null;
-    for(let p of pontos) {
-        if(vmouse.dist(p)<10) {
+
+    // Para todos os pontos, caso a distancia entre o mouse e o currPonto é menor que X... 
+    // Desenha o ponto com a cor de acordo com a distancia entre os elementos.
+    for (let p of pontos) {
+        // Calcula a distancia do ponto com o vetor do mouse
+        if (vmouse.dist(p) < 10) {
             selecionado = p;
             fill("#ff0000");
         } else {
@@ -49,7 +66,7 @@ function desenhaPontos() {
 }
 
 function mouseDragged() {
-    if(selecionado) {
+    if (selecionado) {
         selecionado.set(mouseX, mouseY);
     }
 }
--- Aula1/P5/exemplo1/sketch.js
+++ Aula1/P5/exemplo1/sketch.js
@@ -2,32 +2,34 @@ let radius = 200;
 let n = 3;
 
 function setup() {
-    createCanvas(800,600);
-}  
+    createCanvas(800, 600);
+}
 
 function draw() {
     // Extensão LiveServer e Open In Default Browser
+
+    // Atualiza o canvas com a cor...
     background(200);
     // Muda o ponto de origem do sistema de coordenada
-    translate(width/2,height/2);
+    translate(width / 2, height / 2);
 
-    let [mx,my] = relativeMouse();
+    let [mx, my] = relativeMouse();
 
     // Seta para a direita
-    let v = createVector(1,0);
+    let v = createVector(1, 0);
     // Cria um vetor para a posição do mouse
-    let u = createVector(mx,my);
+    let u = createVector(mx, my);
 
     // Rotaciona o Canvas em uma angulo formado entre os vetores v e u
-    rotate(v.angleBetween(u));
+    // rotate(v.angleBetween(u));
 
     // 
-    n = floor(map(u.mag(),0,width/2, 2, 12));
+    n = floor(map(u.mag(), 0, width / 2, 3 , 12));
 
     beginShape();
-    for(let i=100; i>n; i--) {
-        let angle = map(i,0,n,0,TWO_PI);
-        vertex(radius*cos(angle), radius*sin(angle));
+    for (let i = 0; i < n; i++) {
+        let angle = map(i, 0, n, 0, TWO_PI);
+        vertex(radius * cos(angle), radius * sin(angle));
     }
     endShape(CLOSE);
 }
@@ -35,8 +37,8 @@ function draw() {
 function relativeMouse() {
     let mx = mouseX;
     let my = mouseY;
-	let matrix = drawingContext.getTransform();
-	let pd = pixelDensity();
-    let rp = matrix.inverse().transformPoint(new DOMPoint(mx * pd,my * pd));
-    return [ rp.x, rp.y ];
+    let matrix = drawingContext.getTransform();
+    let pd = pixelDensity();
+    let rp = matrix.inverse().transformPoint(new DOMPoint(mx * pd, my * pd));
+    return [rp.x, rp.y];
 }
