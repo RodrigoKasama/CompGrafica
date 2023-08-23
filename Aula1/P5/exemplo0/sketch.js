ba6c79d54d396471c6c4f34bfc602fce5847d830
RodrigoKasama <rodrigo.parracho@gmail.com>
2023-08-22 21:11:07 -0300

    Aula3

--- Aula1/P5/exemplo0/sketch.js
+++ Aula1/P5/exemplo0/sketch.js
@@ -1,10 +1,54 @@
 
 function setup() {
-    createCanvas(800,600);
-}  
+	createCanvas(800, 600);
+	rectMode(CENTER)
+}
 
 function draw() {
-    background(111,11);
-    rect(mouseX,mouseY, 30, 30);
+	background(200);
+
+	translate(width / 2, height / 2)
+
+	fill("yellow")
+	circle(0, 0, 100)
+
+	drawMercury()
+	drawEarth()
+}
+
+function drawEarth() {
+	push()
+
+	fill("blue")
+	rotate(frameCount * 0.01)
+	circle(width / 4, 0, 50)
+	translate(width / 4, 0)
+
+	drawMoon()
+
+	pop()
+
+}
+
+function drawMoon() {
+	push()
+
+	fill("gray")
+	rotate(frameCount * 0.05)
+	circle(width / 16, 0, 10)
+	translate(width / 4, 0)
+
+	pop()
+}
+
+function drawMercury() {
+	push()
+
+	fill("red")
+	rotate(frameCount * 0.07)
+	circle(width / 8, 0, 5)
+	translate(width / 8, 0)
+
+	pop()
 }
 
